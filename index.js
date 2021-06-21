8bcde7bbaf1448c4d38f9b78c8a3bcef2b49cf42
Bigboy_11111 <79596269+bigboy11111RBLX@users.noreply.github.com>
2021-06-21 17:53:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const fs = require('fs'); //uses fs to look throught the commands folder when looking for command files
+const Discord = require('discord.js'); //gets discord.js
+const MessageEmbed = require('discord.js');
+const { prefix, token } = require('./config.json'); //adds var prefix and token to the code looking via config.json
+const client = new Discord.Client(); // Sets Client var
+require('discord-buttons')(client); // "enables" discord buttons!
+client.commands = new Discord.Collection(); // Makes a collection
+
+//add new "const"'s above this line
+
+// No need to edit below unless you know what you are doing.
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+//---command---
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+//---event---
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
+
+
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.login(token);
